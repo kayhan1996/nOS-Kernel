a696a501228e51d27db3a23a31fbbb90976b3964
Kayhan Baig <kayhan1996@gmail.com>
2020-01-03 17:46:28 -0700

    Added support for clangd debugging

--- src/C/main.c
+++ src/C/main.c
@@ -1,10 +1,10 @@
 
-#include "mmu.h"
 #include "asm.h"
-#include "uart.h"
-#include "process.h"
 #include "mm.h"
+#include "mmu.h"
 #include "printx.h"
+#include "process.h"
+#include "uart.h"
 
 #include "interrupts.h"
 #include "timer.h"
@@ -21,36 +21,36 @@
 extern "C" /* Use C linkage for kernel_main. */
 #endif
 
-void p1(char *str){
-	uint64_t currentEL;
-	printf("Current EL: %d\n", currentEL);
-	while(1){
-		printf(str);
-		delay(1000000);
-	}
+void test_process(char *str) {
+  uint64_t currentEL;
+  printf("Current EL: %d\n", currentEL);
+  while (1) {
+    printf(str);
+    delay(1000000);
+  }
 }
 
+void kernel_main() {
+  init_uart();
+  init_printf(0, putc);
+  printf("Kernel Program Started.\n");
 
+  init_framebuffer();
+  // testCanvas();
 
+  for (int i = 0; i < 15; i++) {
+  	printf("Hello! %d", i);
+  }
 
-void kernel_main(){
-	init_uart();
-	init_printf(0, putc);
-	printf("Kernel Program Started.\n");
+  init_memory_manager();
+  init_process_manager();
+  enable_interrupt_controller();
+  init_arm_timer(300000000);
+  enable_irq();
 
+  create_process(test_process, "abcde\n");
+  create_process(test_process, "     		12345\n");
 
-	init_framebuffer();
-	//testCanvas();
-
-	init_memory_manager();
-	init_process_manager();
-	enable_interrupt_controller();
-	init_arm_timer(300000000);
-	enable_irq();
-
-	create_process(p1, "abcde\n");
-	create_process(p1, "     		12345\n");
-
-	while(1){
-	}
+  while (1) {
+  }
 }
