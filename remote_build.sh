eabbd4d94c9a587617dfbd6af558468da2a3ea6a
Kayhan Baig <kayhan1996@gmail.com>
2020-01-28 19:59:11 -0700

    added extra files to gitignore

--- remote_build.sh
+++ remote_build.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-HOST="kay@192.168.1.96"
+HOST="kay@192.168.1.100"
+PORT=15429
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo -e "${GREEN}Copying Source${NC}"
-ssh ${HOST} "fuser -k 9000/tcp; cd ~; mkdir -p tmp"
-rsync -rzu --stats ../nOS-Kernel/src ${HOST}:~/tmp/ | grep "files transferred"
-rsync -rzu ../nOS-Kernel/include ${HOST}:~/tmp/
-rsync -zu ../nOS-Kernel/Makefile ${HOST}:~/tmp/
-rsync -zu ../nOS-Kernel/linker.ld ${HOST}:~/tmp/
+ssh -p ${PORT} ${HOST} "fuser -k 9000/tcp; cd ~; mkdir -p tmp"
+rsync 'ssh -p 15429' -rzu --stats ../nOS-Kernel/src ${HOST}:~/tmp/ | grep "files transferred"
+rsync 'ssh -p ${PORT}' -rzu ../nOS-Kernel/include ${HOST}:~/tmp/
+rsync 'ssh -p ${PORT}' -zu ../nOS-Kernel/Makefile ${HOST}:~/tmp/
+rsync 'ssh -p ${PORT}' -zu ../nOS-Kernel/linker.ld ${HOST}:~/tmp/
 
 echo -e "${GREEN}Starting Build${NC}"
-ssh ${HOST} "cd ~/tmp; make"
+ssh -p ${PORT} ${HOST} "cd ~/tmp; make"
 
-rsync ${HOST}:~/tmp/build/kernel.sym kernel.sym
+rsync -P ${PORT} ${HOST}:~/tmp/build/kernel.sym kernel.sym
 echo -e "${GREEN}Build Complete${NC}\n"
 
-ssh ${HOST} "cd ~/tmp; make run"
+ssh -p ${PORT} ${HOST} "cd ~/tmp; make run"
--- src/C/Memory/memory_descriptor.c
+++ src/C/Memory/memory_descriptor.c
@@ -6,8 +6,8 @@ void create_process_memory(Memory_Descriptor *md){
     md->stack.start = 0x0000FFFFFFFFFFFF;
     md->stack.size = 0;
 
-    md->heap.end = 0xC800000‬;
-    md->heap.start = 0xC800000‬;
+    md->heap.end = 0xC800000;
+    md->heap.start = 0xC800000;
     md->heap.size = 0;
 
     md->text.end = 0x1000000;
